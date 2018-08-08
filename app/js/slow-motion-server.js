d2c0e7bec163b9d45a391601d78a045dc8fcadbf
Shawn Poellet <39753162+shpoellet@users.noreply.github.com>
2018-08-08 08:36:04 -0500

    downloading working, about in implement connect replys

--- app/js/slow-motion-server.js
+++ app/js/slow-motion-server.js
@@ -1,15 +1,49 @@
-var exec = require('child_process').exec;
-var ffmpeg = require('ffmpeg-static');
+// var exec = require('child_process').exec;
+// var ffmpeg = require('ffmpeg-static');
+//
+// var cmd1 = ffmpeg.path;
+//
+// console.log(cmd1)
+//
+// exec(cmd1, function(error, stdout, stderr){
+//   if(error == null){
+//     console.log("good run")
+//   }
+//   else{
+//     console.log(error)
+//   }
+// })
 
-var cmd1 = ffmpeg.path;
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
 
-console.log(cmd1)
+// function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write('Hello World!');
+//   res.end();
+// }
 
-exec(cmd1, function(error, stdout, stderr){
-  if(error == null){
-    console.log("good run")
-  }
-  else{
-    console.log(error)
-  }
-})
+
+
+function downloadFile(req, res)
+{
+
+  console.log("request")
+  console.log(req.headers);
+  var filePath = path.basename(req.url);
+  var file =  fs.createWriteStream(filePath);
+
+
+  req.pipe(file);
+  req.on('end', function(){
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('Hello World!');
+      res.end();
+      console.log("done")
+  })
+}
+
+
+
+http.createServer(downloadFile).listen(3000);
