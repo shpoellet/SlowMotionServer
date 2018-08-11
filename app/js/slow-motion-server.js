43b42f19faa6e1af16ea6bbf998d3dd4b544de69
Shawn Poellet <39753162+shpoellet@users.noreply.github.com>
2018-08-11 00:51:14 -0500

    figured out ffmpeg sequence

--- app/js/slow-motion-server.js
+++ app/js/slow-motion-server.js
@@ -1,5 +1,5 @@
-// var exec = require('child_process').exec;
-// var ffmpeg = require('ffmpeg-static');
+var exec = require('child_process').exec;
+var ffmpeg = require('ffmpeg-static');
 //
 // var cmd1 = ffmpeg.path;
 //
@@ -22,6 +22,54 @@ var events = require('events').EventEmitter;
 var EventEmitter = new events.EventEmitter();
 
 
+var cmdStart = ffmpeg.path + ' -i start.mov -f mp4 -vcodec libx264 -acodec aac -s 1280x720 -r 24 -y intro.mp4';
+var cmdEnd = ffmpeg.path + ' -i end.mpeg -f mp4 -vcodec libx264 -acodec aac -s 1280x720 -r 24 -y exit.mp4';
+
+exec(cmdStart, function(error, stdout, stderr){
+  if(error == null){
+    console.log("good run")
+  }
+  else{
+    console.log(error)
+  }
+})
+
+
+
+exec(cmdEnd, function(error, stdout, stderr){
+  if(error == null){
+    console.log("good run")
+  }
+  else{
+    console.log(error)
+  }
+})
+
+
+function convert(filePath){
+  // var cmd1 = ffmpeg.path + ' -i '+ filePath + ' -c copy -bsf:v h264_mp4toannexb -f mpegts intermediate1.ts';
+
+  var cmd1 = ffmpeg.path + ' -i ' + filePath + ' -f mp4 -vcodec libx264 -acodec aac -s 1280x720 -r 24 -y out.mp4';
+  exec(cmd1, function(error, stdout, stderr){
+    if(error == null){
+      console.log("good run")
+      // var cmd2 = ffmpeg.path + ' -i intro.mp4 -i out.mp4 -i exit.mp4 -filter_complex "[0:v:0][0:a:0][1:v:0][1:a:0][2:v:0][2:a:0]concat=n=3:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" output.mp4'
+      // exec(cmd1, function(error, stdout, stderr){
+      //   if(error == null){
+      //     console.log("done")
+      //   }
+      //   else{
+      //     console.log(error)
+      //   }
+      // })
+
+    }
+    else{
+      console.log(error)
+    }
+  })
+}
+
 
 function processRequest(req, res)
 {
@@ -41,6 +89,7 @@ function processRequest(req, res)
         res.end();
         console.log("Download Complete")
         EventEmitter.emit('newFile', req.url);
+        convert(filePath);
     })
   }
   else{
